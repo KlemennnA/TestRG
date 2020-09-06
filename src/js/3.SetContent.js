09bf76ffe864990ea3a385001b8cfab54d9a73f1
KlemennnA <antolin.klemen@gmail.com>
2020-09-06 15:48:39 +0200

    1

---
+++ src/js/3.SetContent.js
@@ -0,0 +1,167 @@
+var aboutUs = "O nas";
+var news = "Novice";
+var contacts = "Kontakt";
+var footerLinksTitle = "NAŠE STORITVE";
+var address = "Šmartinska 130,<br>SI-1000 Ljubljana";
+var phone = "+386 1 350 71 60";
+var mail = "info@companymail.si";
+
+var title1 = "Lorem ipsum dolor sit amet";
+var title2 = "Lorem ipsum dolor sit amet";
+var title3 = "POSLOVNA ODLIČNOST";
+
+var subtitle1 = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione optio voluptatum corporis consectetur deserunt maiores molestiae totam autem consequuntur tempora!";
+var subtitle2 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, illum aperiam natus vel recusandae ad commodi laboriosam eligendi minima aut.";
+
+var leftColumnTitle = "Vprašajte nas";
+var leftColumnText = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum velit ut ad cumque impedit iusto, dolorum recusandae reiciendis labore quia veniam, esse saepe aspernatur repudiandae alias similique. Pariatur, minima ipsa.";
+
+var tab1 = "PONUDBA";
+var tab2 = "SLEDITE";
+
+var tab1text = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem doloribus officia repudiandae, numquam quia ratione eos amet dolorem quos animi.";
+var tab2text = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, odit.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, odit.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, odit.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, odit.";
+
+var input1Label = "Od:";
+var input2Label = "Do:";
+var input3Label = "Reg. št:";
+
+var advantage1Title = "01";
+var advantage2Title = "02";
+var advantage3Title = "03";
+var advantage4Title = "04";
+
+var advantage1Subtitle = "VEČ KOT 50 LET TRADICIJE";
+var advantage2Subtitle = "HITRO IN ZANESLJIVO";
+var advantage3Subtitle = "PRILAGOJENE REŠITVE";
+var advantage4Subtitle = "LOGISTIKA A-Ž";
+
+var advantage1Text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, fuga.";
+var advantage2Text = advantage1Text;
+var advantage3Text = advantage1Text;
+var advantage4Text = advantage1Text;
+
+var smallAdvantage1Title = "01";
+var smallAdvantage2Title = "02";
+var smallAdvantage3Title = "03";
+var smallAdvantage4Title = "04";
+
+var smallAdvantage1Subtitle = "VEČ KOT 50 LET TRADICIJE";
+var smallAdvantage2Subtitle = "HITRO IN ZANESLJIVO";
+var smallAdvantage3Subtitle = "PRILAGOJENE REŠITVE";
+var smallAdvantage4Subtitle = "LOGISTIKA A-Ž";
+
+var smallAdvantage1Text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, fuga.";
+var smallAdvantage2Text = smallAdvantage1Text;
+var smallAdvantage3Text = smallAdvantage1Text;
+var smallAdvantage4Text = smallAdvantage1Text;
+
+var buttonOffer = "PRIDOBI PONUDBO";
+var buttonTrack = "SLEDITE";
+var button1 = "Lorem<br>ipsum";
+var button2 = button1;
+var button3 = button1;
+var button4 = button1;
+var button5 = button1;
+var button6 = button1;
+var button7 = button1;
+
+var smallButton1 = button1.replace("<br>", " ");
+var smallButton2 = button2.replace("<br>", " ");
+var smallButton3 = button3.replace("<br>", " ");
+var smallButton4 = button4.replace("<br>", " ");
+var smallButton5 = button5.replace("<br>", " ");
+var smallButton6 = button6.replace("<br>", " ");
+var smallButton7 = button7.replace("<br>", " ");
+
+var footerLink1 = "Železna logistika";
+var footerLink2 = "Pomorska logistika";
+var footerLink3 = "Cestna logistika";
+var footerLink4 = "Letalska logistika";
+var footerLink5 = "Blagovne storitve";
+var footerLink6 = "Administrativne logistike";
+var footerLink7 = "Skladiščenje";
+
+
+let allStrings = 
+[
+    {aboutUs: aboutUs},
+    {news: news},
+    {contacts: contacts},
+    {footerLinksTitle: footerLinksTitle},
+    {footerContacts: contacts},
+    {address: address},
+    {phone: phone},
+    {mail: mail},
+    {title1: title1},
+    {title2: title2},
+    {title3: title3},
+    {subtitle1: subtitle1},
+    {subtitle2: subtitle2},
+    {leftColumnTitle: leftColumnTitle},
+    {leftColumnText: leftColumnText},
+    {tab1: tab1},
+    {tab2: tab2},
+    {tab1text: tab1text},
+    {tab2text: tab2text},
+    {input1Label: input1Label},
+    {input2Label: input2Label},
+    {input3Label: input3Label},
+    {advantage1Title: advantage1Title},
+    {advantage2Title: advantage2Title},
+    {advantage3Title: advantage3Title},
+    {advantage4Title: advantage4Title},
+    {advantage1Subtitle: advantage1Subtitle},
+    {advantage2Subtitle: advantage2Subtitle},
+    {advantage3Subtitle: advantage3Subtitle},
+    {advantage4Subtitle: advantage4Subtitle},
+    {advantage1Text: advantage1Text},
+    {advantage2Text: advantage2Text},
+    {advantage3Text: advantage3Text},
+    {advantage4Text: advantage4Text},
+    {smallAdvantage1Title: smallAdvantage1Title},
+    {smallAdvantage2Title: smallAdvantage2Title},
+    {smallAdvantage3Title: smallAdvantage3Title},
+    {smallAdvantage4Title: smallAdvantage4Title},
+    {smallAdvantage1Subtitle: smallAdvantage1Subtitle},
+    {smallAdvantage2Subtitle: smallAdvantage2Subtitle},
+    {smallAdvantage3Subtitle: smallAdvantage3Subtitle},
+    {smallAdvantage4Subtitle: smallAdvantage4Subtitle},
+    {smallAdvantage1Text: smallAdvantage1Text},
+    {smallAdvantage2Text: smallAdvantage2Text},
+    {smallAdvantage3Text: smallAdvantage3Text},
+    {smallAdvantage4Text: smallAdvantage4Text},
+    {buttonOffer: buttonOffer},
+    {buttonTrack: buttonTrack},
+    {button1: button1},
+    {button2: button2},
+    {button3: button3},
+    {button4: button4},
+    {button5: button5},
+    {button6: button6},
+    {button7: button7},
+    {smallButton1 : smallButton1},
+    {smallButton2 : smallButton2},
+    {smallButton3 : smallButton3},
+    {smallButton4 : smallButton4},
+    {smallButton5 : smallButton5},
+    {smallButton6 : smallButton6},
+    {smallButton7 : smallButton7},
+    {footerLink1: footerLink1},
+    {footerLink2: footerLink2},
+    {footerLink3: footerLink3},
+    {footerLink4: footerLink4},
+    {footerLink5: footerLink5},
+    {footerLink6: footerLink6},
+    {footerLink7: footerLink7}
+];
+function setContent(){
+    allStrings.forEach(obj => {
+        if(document.body.contains(document.getElementById(Object.getOwnPropertyNames(obj)))){
+            document.getElementById(Object.getOwnPropertyNames(obj)).innerHTML = obj[Object.getOwnPropertyNames(obj)];
+        }
+        else{
+            console.log(Object.getOwnPropertyNames(obj) + " is missing");
+        };
+    });
+};
